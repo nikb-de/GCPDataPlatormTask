f30a5b03dd058c4a4573e88594079c56c53eb1fb
Nikita Bakanchev <bakanchevn@gmail.com>
2024-08-21 22:54:14 +0200

    Added terraform pub/sub

---
+++ tf/main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  project = var.project_id
+  region = var.region
+  credentials = var.credentials_file
+}
+
+resource "google_pubsub_topic" "offer" {
+  name = "offer"
+}
+
+resource "google_pubsub_topic" "user_offers" {
+  name = "user_offers"
+}
+
+resource "google_pubsub_topic" "offer_tasks" {
+  name = "offer_tasks"
+}
+
+variable "project_id" {
+  description = "The ID of the GCP project"
+}
+
+variable "region" {
+  description = "The GCP region"
+  default = "europe-west10"
+}
+
+variable "credentials_file" {
+  description = "Path to the service account key file"
+}
+
+
+
+
+
